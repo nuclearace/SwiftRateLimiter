a6d34d5e73eccdfb90f9a3703f321d1db983b64c
Erik Little <nuclear.ace@gmail.com>
2019-12-01 10:41:18 -0500

    update for latest swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,13 @@
+// swift-tools-version:5.1
+
 import PackageDescription
 
 let package = Package(
-    name: "SwiftRateLimiter"
+    name: "SwiftRateLimiter",
+    products: [
+        .library(name: "SwiftRateLimiter", targets: ["SwiftRateLimiter"])
+    ],
+    targets: [
+        .target(name: "SwiftRateLimiter")
+    ]
 )
